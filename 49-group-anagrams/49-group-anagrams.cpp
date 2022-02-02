05e5fe77532f43ca6ff63a2cb4ebf9c43ecaee20
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-02-02 14:02:57 +0530

    Time: 1234 ms (5.02%), Space: 17.7 MB (98.16%) - LeetHub

---
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<string> copy(strs);
+        for(auto &s:copy) sort(s.begin(), s.end());
+        vector<vector<string>> ans;
+        for(int i=0;i<copy.size();i++){
+            vector<string> sans;
+            string curr=copy[i];
+            if(curr=="VISITED") continue;
+            for(int j=0;j<copy.size();j++){
+                if(copy[j]==curr){
+                    sans.push_back(strs[j]);
+                    copy[j]="VISITED";
+                }
+            }
+            ans.push_back(sans);
+        }
+        return ans;
+    }
+};
