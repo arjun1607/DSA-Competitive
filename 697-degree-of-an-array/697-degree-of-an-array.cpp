a42223e86a480a3de4be91008e05c990a9885884
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-02-04 19:20:05 +0530

    Time: 40 ms (77.60%), Space: 29.2 MB (13.52%) - LeetHub

--- 697-degree-of-an-array/697-degree-of-an-array.cpp
+++ 697-degree-of-an-array/697-degree-of-an-array.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int findShortestSubArray(vector<int>& nums) {
-        unordered_map<int,int> m; for(int i:nums) m[i]++;
-        int f=INT_MIN; 
+        unordered_map<int,vector<int>> m; 
+        for(int i=0;i<nums.size();i++) m[nums[i]].push_back(i);
+        int f=0; 
         vector<int> v;
         for(auto i:m){
-            if(i.second > f){
-                f=i.second;
+            if(i.second.size() > f){
+                f=i.second.size();
                 v.clear();
                 v.push_back(i.first);
-            }else if(i.second == f) v.push_back(i.first);
-        }
+            }else if(i.second.size() == f) v.push_back(i.first);
+        }      
         int ans=INT_MAX;
         for(int val:v){
-            int i=0,j=0; bool first=false;
-            for(int k=0;k<nums.size();k++){
-                if(nums[k]==val){
-                    j=k;
-                    if(first==false){
-                        i=k; first=true;
-                    }
-                }
-            }
-            ans=min(ans,j-i+1);
+            cout<<val<<" ";
+            ans=min(ans,m[val].back()-m[val].front()+1);
         }
         return ans;
     }
