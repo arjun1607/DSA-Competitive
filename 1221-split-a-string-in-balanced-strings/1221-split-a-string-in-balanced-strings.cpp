bd156910d0b1f22c8c1bc0eebb941ddd70199af6
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-02-04 23:44:45 +0530

    Time: 3 ms (25.35%), Space: 6.2 MB (79.51%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     int balancedStringSplit(string s) {
-        vector<int> pre(s.size(),0);
-        pre[0]=(s[0]=='L')?1:0;
-        for(int i=1;i<s.size();i++){
-            if(s[i]=='L') pre[i]=pre[i-1]+1;
-            else pre[i]=pre[i-1];
+        // BALANCE VARIABLE TRICK
+        int val=0, count=0;
+        for(char c:s){
+            if(c=='L') val++;
+            else if(c=='R') val--;
+            if(val==0) count++;
         }
-        int c=0, ans=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='R') c++;
-            if(c==pre[i]) ans++;
-        }
-        return ans;
+        return count;
     }
 };
