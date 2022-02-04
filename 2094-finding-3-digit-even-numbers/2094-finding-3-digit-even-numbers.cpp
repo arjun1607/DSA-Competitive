9a7e9f912867888afb1a2a443179e670b1b47f5b
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-02-04 15:33:10 +0530

    Time: 50 ms (44.71%), Space: 12.3 MB (18.16%) - LeetHub

--- 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& d) {
+        // Check for all numbers between 100 to 999 whether it can be formed by the digits present in digits vector
         unordered_map<int,int> m; for(int i:d) m[i]++;
         vector<int> ans;
-        for(int i=100;i<=999;i++){
+        for(int i=100;i<=999;i+=2){
             vector<int> used;
-            bool flag=true;
             int n=i;
             while(n!=0){
                 if(m[n%10]>0){
                     used.push_back(n%10);
                     m[n%10]--;
-                }else{
-                    flag=false;
-                    break;
-                }
+                }else break;
                 n=n/10;
             }
-            if(flag && i%2==0) ans.push_back(i);
+            if(used.size()==3) ans.push_back(i);
             for(int j:used) m[j]++;
         }
         sort(ans.begin(), ans.end());
