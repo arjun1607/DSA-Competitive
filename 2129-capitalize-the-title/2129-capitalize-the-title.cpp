dc3fab027190bef43b8bd8cabcebe9a743a526d4
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-02-05 12:22:28 +0530

    Time: 4 ms (51.82%), Space: 6.3 MB (52.28%) - LeetHub

--- 2129-capitalize-the-title/2129-capitalize-the-title.cpp
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -4,13 +4,14 @@ public:
         int i=0;
         for(int j=0;j<s.size();j++){
             if(s[j]==' '){
-                i=j+1;
-                
+                i=j+1;               
             }
-            else if(j-i+1>2){
-                s[i]=toupper(s[i]);
+            else{
+                s[j]=tolower(s[j]);
+                if(j-i+1>2){
+                    s[i]=toupper(s[i]);
+                }
             }
-            s[j]=tolower(s[j]);
         }
         return s;
     }
