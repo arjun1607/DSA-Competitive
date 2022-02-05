6879c1bac1a1885668c09d2d3fe4bb36ad6560af
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-02-05 15:03:40 +0530

    Time: 89 ms (23.13%), Space: 19.6 MB (85.19%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     bool validPalindrome(string s) {
-        for(int i=0,j=s.size()-1;i<=j;i++,j--){
+        /*
+        Check from left and right at the same time until the first different pair.
+Now we have something like a****b, where a and b are different.
+We need to delete either a or b to make it a palindrome.
+        */
+        for(int i=0,j=s.size()-1;i<j;i++,j--){
             if(s[i]!=s[j]){
-                int p=i+1, q=j, u=i, v=j-1;
-                while(p<=q && s[p]==s[q]){p++; q--;}
-                while(u<=v && s[u]==s[v]){u++; v--;}
-                if(p>q || u>v) return true;
-                else if(p<q && u<v) return false;
+                int p=i,q=j-1,u=i+1,v=j;
+                while(p<q && s[p]==s[q]){p++; q--;}
+                while(u<v && s[u]==s[v]){u++; v--;}
+                return p>=q || u>=v;
             }
         }
         return true;
     }
 };
-
-
-
-
-
