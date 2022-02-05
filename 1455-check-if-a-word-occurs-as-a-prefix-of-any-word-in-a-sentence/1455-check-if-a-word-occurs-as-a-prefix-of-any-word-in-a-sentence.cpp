d7ab898ca8d421273d7a9a12d06ce6c9b6700e59
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-02-05 13:41:53 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (58.69%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -1,20 +1,51 @@
 class Solution {
 public:
     int isPrefixOfWord(string s, string t) {
+        /* TWO POINTERS
+        
         int j=0, count=0;
         for(int i=0;i<s.size();){
             if(s[i]!=' '){
-                if(s[i]==t[j]) { i++; j++; }
-                else{
+                if(s[i]==t[j]) { 
+                    i++; j++; 
+                }else{
                     while(i<s.size() && s[i]!=' ') i++;
                 }
                 if(j==t.size()) return count+1;
             }else {
+                
+                    Input: "b bu bur burg burger" , "burg"
+                    Expected: 4
+                
                 count++;
                 i++;
                 j=0;
             }
         }
         return -1;
+        */     
+        
+           // ADDING LEADING SPACES TRICK
+           s=" "+s; t=" "+t;
+           auto pos=s.find(t);
+           if(pos!=string::npos) return count(s.begin(), s.begin()+pos+1, ' ');
+           return -1;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
