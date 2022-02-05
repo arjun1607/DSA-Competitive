8af93b7e0bcf0d4da21e526db147a8923d266344
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-02-05 12:19:53 +0530

    Time: 4 ms (51.82%), Space: 6.2 MB (79.16%) - LeetHub

---
+++ 2129-capitalize-the-title/2129-capitalize-the-title.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string capitalizeTitle(string s) {
+        int i=0;
+        for(int j=0;j<s.size();j++){
+            if(s[j]==' '){
+                i=j+1;
+            }
+            else if(j-i+1>2){
+                s[i]=toupper(s[i]);
+            }
+            s[j]=tolower(s[j]);
+        }
+        return s;
+    }
+};
