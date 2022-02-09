dc2102e1d9fd9932a9584a05f50864e20131c887
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-02-09 16:38:15 +0530

    Time: 46 ms (64.82%), Space: 24.6 MB (97.52%) - LeetHub

--- 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.cpp
+++ 961-n-repeated-element-in-size-2n-array/961-n-repeated-element-in-size-2n-array.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& arr) {
+        // CLEVER OBSERVATION
         int ans;
-        for(int i=0;i<arr.size()-3;i++){
-            if(arr[i]==arr[i+1] || arr[i]==arr[i+2] || arr[i]==arr[i+3]){ 
-                ans=arr[i]; break;
-            }else if(arr[i+1]==arr[i+2] || arr[i+1]==arr[i+3]){
-                ans=arr[i+1]; break;
-            }else if(arr[i+2]==arr[i+3]){
-                ans=arr[i+2]; break;
+        // for(int i=0;i<arr.size()-3;i++){
+        //     if(arr[i]==arr[i+1] || arr[i]==arr[i+2] || arr[i]==arr[i+3]){ 
+        //         ans=arr[i]; break;
+        //     }else if(arr[i+1]==arr[i+2] || arr[i+1]==arr[i+3]){
+        //         ans=arr[i+1]; break;
+        //     }else if(arr[i+2]==arr[i+3]){
+        //         ans=arr[i+2]; break;
+        //     }
+        // }
+        for(int k=1;k<=3;k++){
+            for(int i=0;i<arr.size()-k;i++){
+                if(arr[i]==arr[i+k]){
+                    ans=arr[i]; break;
+                }
             }
         }
         return ans;
