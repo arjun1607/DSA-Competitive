2a06f630fa9d0b125a5a42e050919e3f0b36f660
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-03-11 13:54:00 +0530

    Time: 7 ms (95.96%), Space: 10.5 MB (39.02%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> dp(nums.size());
+        /* vector<int> dp(nums.size());
         dp[0]=1;
         for(int i=1;i<nums.size();i++){
             int ans=1;
@@ -13,5 +13,24 @@ public:
             dp[i]=ans;
         }
         return *max_element(dp.begin(), dp.end());
+        */
+        // USING BINARY SEARCH
+        vector<int> dp(nums.size(), INT_MAX);
+        int ans=0;
+        for(int val:nums){
+            int i=0, j=ans;
+            int pos=-1;
+            while(i<=j){
+                int mid=i+(j-i)/2;
+                if(dp[mid]==val) {
+                    pos=mid; break;
+                }else if(dp[mid]>val){
+                    pos=mid; j=mid-1;
+                }else i=mid+1;
+            }
+            dp[pos]=val;
+            if(pos==ans) ans++;
+        }
+        return ans;
     }
 };
