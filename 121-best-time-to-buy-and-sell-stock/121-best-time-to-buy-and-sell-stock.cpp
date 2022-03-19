48079d29eef0a4f6436d7e1bbb012c12e9c312a3
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-03-19 11:34:52 +0530

    Time: 164 ms (62.50%), Space: 97.3 MB (5.75%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& p) {
-        vector<int> min(p.size()), m(p.size());
-        min[0]=p[0]; m[p.size()-1]=p[p.size()-1];
-        for(int i=1;i<p.size();i++) min[i]=(p[i]<min[i-1])?p[i]:min[i-1];    
-        for(int i=p.size()-2;i>=0;i--) m[i]=(p[i]>m[i+1])?p[i]:m[i+1];
-        int ans=0;
-        for(int i=0;i<p.size();i++) ans=max(ans, m[i]-min[i]);
-        return ans;
+        // vector<int> min(p.size()), m(p.size());
+        // min[0]=p[0]; m[p.size()-1]=p[p.size()-1];
+        // for(int i=1;i<p.size();i++) min[i]=(p[i]<min[i-1])?p[i]:min[i-1];    
+        // for(int i=p.size()-2;i>=0;i--) m[i]=(p[i]>m[i+1])?p[i]:m[i+1];
+        // int ans=0;
+        // for(int i=0;i<p.size();i++) ans=max(ans, m[i]-min[i]);
+        // return ans;
+        
+        int MIN=p[0];
+        vector<int> dp(p.size());
+        for(int i=1;i<p.size();i++){
+            dp[i]=(p[i] > MIN)?(p[i]-MIN):0;
+            MIN=min(MIN, p[i]);
+        }
+        return *max_element(dp.begin(), dp.end());
     }
 };
