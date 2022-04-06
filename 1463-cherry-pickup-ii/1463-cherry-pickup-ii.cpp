5c5a68b55e837149ae1cb0de81b1831667bab6a7
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-04-06 08:55:19 +0530

    Time: 84 ms (63.36%), Space: 9.2 MB (88.07%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -28,10 +28,44 @@ public:
     }
     
     int cherryPickup(vector<vector<int>>& grid) {
-        // dp[m][n][n]
+                
         int m=grid.size(), n=grid[0].size();
+        /* MEMOIZATION
         vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(n, -1)));
         return memo(0, 0, n - 1, grid, m, n, dp);
+        */
+        
+        int dp[m][n][n];
+        // base case
+        for(int j1=0;j1<n;j1++){
+            for(int j2=0;j2<n;j2++){
+                if (j1 == j2) dp[m-1][j1][j2] = grid[m-1][j1];
+                else dp[m-1][j1][j2] = grid[m-1][j1] + grid[m-1][j2];
+            }
+        }
+        //. tabualtion is BOTTOM UP -> from base case to up -> from row n-1 to 0
+        for(int i=m-2;i>=0;i--){
+            for(int j1=0;j1<n;j1++){
+                for(int j2=0;j2<n;j2++){
+                    int maxi = INT_MIN;
+                    for (int dj1 = -1; dj1 <= 1; dj1++) {
+                        for (int dj2 = -1; dj2 <= 1; dj2++) {
+                          int val=0;
+                          if (j1 == j2) val = grid[i][j1]; 
+                          else val = grid[i][j1] + grid[i][j2]; 
+                            
+                          if(j1+dj1 >=0 && j1+dj1<n && j2+dj2 >=0 && j2+dj2<n) 
+                              val += dp[i+1][j1+dj1][j2+dj2];
+                          else val += -1e8;
+                            
+                          maxi = max(maxi, val);
+                        }
+                      }
+                   dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+        return dp[0][0][n-1];
     }
 };
 
