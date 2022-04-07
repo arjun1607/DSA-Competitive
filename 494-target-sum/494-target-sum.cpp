94d9ee4f7417221d2c00a6fa8b0818917e10fc46
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-04-07 15:53:31 +0530

    Time: 294 ms (35.03%), Space: 54.1 MB (18.18%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int memo(int i, int target, vector<int> &nums){
+    int memo(int i, int target, vector<int> &nums, vector<unordered_map<int,int>> &dp){
         if(i==0){
             if(nums[0]==target || -1*nums[0]==target){
                 if(nums[0]==0) return 2;
@@ -8,12 +8,19 @@ public:
             }
             return 0;
         }
-        int plus = memo(i-1, target-nums[i], nums);
-        int minus = memo(i-1, target+nums[i], nums);
-        return plus + minus;
+        
+        if(dp[i].find(target)!=dp[i].end()) return dp[i][target];
+        
+        // take the element at ith idx with + sign, so the target we are left with is 'target' - nums[i]
+        int plus = memo(i-1, target-nums[i], nums, dp);
+        // similarly when we take with - sign
+        int minus = memo(i-1, target+nums[i], nums, dp);
+        
+        return dp[i][target] = plus + minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        return memo(n-1, target, nums);
+        vector<unordered_map<int,int>> dp(n);
+        return memo(n-1, target, nums, dp);
     }
 };
