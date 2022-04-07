fe5f753ee689aa2d64176163565f1a0c4098f6c5
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-04-07 13:51:36 +0530

    Time: 116 ms (52.03%), Space: 45.1 MB (20.54%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -17,8 +17,45 @@ public:
     }
     int coinChange(vector<int>& coins, int target) {
         int n=coins.size();
+        /*
         vector<vector<int>> dp(n, vector<int>(target+1, -1));
         int ans=memo(n-1, target, coins, dp);
         return (ans>=1e9)?-1:ans;
+        */
+        
+        vector<vector<int>> dp(n, vector<int>(target+1));
+        for(int T=0;T<=target;T++){
+            if(T%coins[0] == 0) dp[0][T]=T/coins[0];
+            else dp[0][T]=1e9;
+        }
+        for(int i=0;i<n;i++) dp[i][0]=0;
+        for(int i=1;i<n;i++){
+            for(int T=1;T<=target;T++){
+                int nottake = 0 + dp[i-1][T];
+                int take =INT_MAX;
+                if(coins[i]<=T) take = 1 + dp[i][T-coins[i]];
+                dp[i][T] = min(take, nottake);
+            }
+        }
+        return (dp[n-1][target]>=1e9)?-1:dp[n-1][target];
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
