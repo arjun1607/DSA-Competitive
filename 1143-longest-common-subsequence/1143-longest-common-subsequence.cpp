64e4d5ded8a92f043931d1f51c9731f60937c05d
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-04-08 12:43:59 +0530

    Time: 24 ms (61.96%), Space: 6.4 MB (98.32%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int memo(int i, int j, string &s, string &t, vector<vector<int>> &dp){
-        if(i==0||j==0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i-1]==t[j-1])
-            return dp[i][j]=1+memo(i-1, j-1, s, t, dp);
-        else 
-            return dp[i][j]=max(memo(i-1, j, s, t, dp), memo(i, j-1, s, t, dp));
-    }
+    // int memo(int i, int j, string &s, string &t, vector<vector<int>> &dp){
+    //     if(i==0||j==0) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(s[i-1]==t[j-1])
+    //         return dp[i][j]=1+memo(i-1, j-1, s, t, dp);
+    //     else 
+    //         return dp[i][j]=max(memo(i-1, j, s, t, dp), memo(i, j-1, s, t, dp));
+    // }
     int longestCommonSubsequence(string s, string t) {
         int n=s.size(), m=t.size();
         /* MEMOIZATION
