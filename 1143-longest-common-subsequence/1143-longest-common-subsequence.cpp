3166bcb932ff517ee611de6235d01fa25e0784be
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-04-08 11:15:40 +0530

    Time: 28 ms (53.30%), Space: 6.5 MB (96.59%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -31,7 +31,7 @@ public:
         return dp[n][m];
         */
         
-        // SPACE OPTIMISED
+        /* SPACE OPTIMISED
         vector<int> prev(m+1), curr(m+1);
         for(int j=0;j<=m;j++) prev[j]=0;
         for(int i=1;i<=n;i++){
@@ -45,6 +45,27 @@ public:
             prev=curr;
         }
         return curr[m];
+        */
+        
+        
+        //EVEN MORE OPTIMISED 
+        vector<int> prev(m+1, 0);
+        for(int j=0;j<=m;j++) prev[j]=0;
+        for(int i=1;i<=n;i++){
+            int prv=0, currp=0;
+            for(int j=1;j<=m;j++){    
+                int p=prev[j];
+                
+                if(s[i-1]==t[j-1])
+                    prev[j]= 1+prv;
+                else
+                    prev[j]=max(prev[j], currp);    
+                
+                prv=p;
+                currp=prev[j];
+            }
+        }
+        return prev[m];
         
     }
 };
