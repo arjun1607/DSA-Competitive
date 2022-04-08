f5fe2325ee146a1fd6bc753d66dc812df18ae275
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-04-08 10:43:40 +0530

    Time: 13 ms (84.26%), Space: 6.4 MB (98.32%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -10,20 +10,41 @@ public:
     }
     int longestCommonSubsequence(string s, string t) {
         int n=s.size(), m=t.size();
-        // vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
-        // return memo(n, m, s, t, dp);
+        /* MEMOIZATION
+           vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+           return memo(n, m, s, t, dp);
+        */
+        
+        /* TABUALR
         int dp[n+1][m+1];
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
                 if(i==0 || j==0) dp[i][j]=0;
                 else{              
                     if(s[i-1]==t[j-1])
-                      dp[i][j]= 1+dp[i-1][j-1];
+                       dp[i][j]= 1+dp[i-1][j-1];
                     else
                        dp[i][j]=max(dp[i-1][j],dp[i][j-1]);            
                 }
             }
         }
         return dp[n][m];
+        */
+        
+        // SPACE OPTIMISED
+        vector<int> prev(m+1), curr(m+1);
+        for(int j=0;j<=m;j++) prev[j]=0;
+        for(int i=1;i<=n;i++){
+            curr[0]=0;
+            for(int j=1;j<=m;j++){              
+                if(s[i-1]==t[j-1])
+                    curr[j]= 1+prev[j-1];
+                else
+                    curr[j]=max(prev[j], curr[j-1]);            
+            }
+            prev=curr;
+        }
+        return curr[m];
+        
     }
 };
