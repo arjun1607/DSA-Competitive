6bab5508f2542497bb9d4adaba4d032f3b84569c
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-04-09 16:38:39 +0530

    Time: 64 ms (45.58%), Space: 26.9 MB (46.65%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-     int lps(string s) {
-        int n=s.size();
-        vector<int> front(n, 0), curr(n, 0);
-        for(int i=n-1;i>=0;i--){
-            curr[i]=1;
-            for(int j=i+1;j<n;j++){
-                  if(s[i]==s[j])  curr[j]=2+front[j-1]; 
-                  else curr[j] = max(front[j], curr[j-1]);
-            }
-            front=curr;
-        }
-        return front[n-1];
-    
+    int memo(int i, int j, string &s, vector<vector<int>> &dp){
+        if(j<=i) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        // if same no need to delete
+        // else either delete ith char or jth char
+        if(s[i]==s[j]) return dp[i][j]=memo(i+1, j-1, s, dp);
+        else return dp[i][j]=1+min(memo(i+1,j,s,dp), memo(i,j-1,s,dp));
+            
     }
     int minInsertions(string s) {
-        return s.size()-lps(s);
+        int n=s.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return memo(0, n-1, s, dp);
     }
 };
