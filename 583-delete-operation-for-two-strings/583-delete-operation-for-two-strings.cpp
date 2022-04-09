a004daa523fc9999c8bc5847e6222114df1bb40f
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-04-09 20:52:07 +0530

    Time: 39 ms (31.78%), Space: 12.4 MB (15.80%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int memo(int i, int j, string &s, string &t, vector<vector<int>> &dp){
-        if(i==0||j==0) return 0;
+        if(i==0 && j==0) return 0;
+        else if(i==0) return j;
+        else if(j==0) return i;
+        
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i-1]==t[j-1])
-            return dp[i][j]=1+memo(i-1, j-1, s, t, dp);
-        else 
-            return dp[i][j]=max(memo(i-1, j, s, t, dp), memo(i, j-1, s, t, dp));
+        
+        if(s[i-1]==t[j-1]) return dp[i][j]=memo(i-1, j-1, s, t, dp);
+        else return dp[i][j]= 1 + min(memo(i-1, j, s, t, dp), memo(i, j-1, s, t ,dp));
     }
     int minDistance(string s, string t) {
         int n=s.size(), m=t.size();
         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        int lcs=memo(n, m, s, t, dp);
-        return n+m-2*lcs;
+        return memo(n, m, s, t, dp);
     }
 };
