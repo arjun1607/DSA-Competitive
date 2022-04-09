ea1fb8010c5619e4df726a18e079d57f87f2b117
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-04-09 13:56:22 +0530

    Time: 117 ms (60.69%), Space: 6.7 MB (96.70%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.cpp
@@ -13,25 +13,39 @@ public:
     }
     int longestPalindromeSubseq(string s) {
         int n=s.size();
-        /*
+        /* MEMOIZATION  memo(i,j) -> it returns len of lps of string starting at index i and ending at index j
         vector<vector<int>> dp(n, vector<int>(n,-1));
         return memo(0, n-1, s, dp);
         */
         
-        // TABULATION -> from BOTTOM to UP -> from BASE CASE to memo call
-        int dp[n][n];
+        /* TABULATION -> from BOTTOM to UP -> from BASE CASE to memo call
+        vector<vector<int>> &dp(n, vector<int>(n));
         for(int i=n-1;i>=0;i--){
             for(int j=0;j<n;j++){
                 if(j<i) dp[i][j]=0;
                 else if(j==i) dp[i][j]=1;
                 else{
-                // if same -> both are part of lps
-                // if different only one can be part of lps
                   if(s[i]==s[j])  dp[i][j]=2+dp[i+1][j-1]; 
                   else dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
                 }
             }
         }
         return dp[0][n-1];
+        */
+        
+        // SPACE OPTIMISED
+        vector<int> front(n, 0), curr(n, 0);
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<n;j++){
+                if(j<i) curr[j]=0;
+                else if(j==i) curr[j]=1;
+                else{
+                  if(s[i]==s[j])  curr[j]=2+front[j-1]; 
+                  else curr[j] = max(front[j], curr[j-1]);
+                }
+            }
+            front=curr;
+        }
+        return front[n-1];
     }
 };
