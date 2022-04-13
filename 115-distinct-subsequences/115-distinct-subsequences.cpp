488c08c9ffeacce7d40c619662558012943406de
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-04-13 08:50:04 +0530

    Time: 15 ms (95.26%), Space: 15 MB (68.58%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -11,7 +11,23 @@ public:
     }
     int numDistinct(string s1, string s2) {
         int n=s1.size(), m=s2.size();
+        /*
         vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
         return memo(n, m, s1, s2, dp);
+        */
+        long dp[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(j==0) dp[i][j]=1;
+                else if(i==0) dp[i][j]=0;
+                else{
+                    if(s2[j-1]==s1[i-1])
+                        dp[i][j] = int(dp[i-1][j-1] + dp[i-1][j]); 
+                    else 
+                        dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
