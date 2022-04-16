3c392aeeac27f134ed25b18d02c2546873664af7
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-04-16 10:44:19 +0530

    Time: 11 ms (99.12%), Space: 18.6 MB (49.19%) - LeetHub

--- 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
+++ 845-longest-mountain-in-array/845-longest-mountain-in-array.cpp
@@ -2,19 +2,15 @@ class Solution {
 public:
     int longestMountain(vector<int>& nums) {
         int n=nums.size();
+        /*
         vector<int> dp1(n,1), dp2(n,1);
         for(int i=1;i<n;i++)
             if(nums[i]>nums[i-1])
                 dp1[i]=dp1[i-1]+1;           
         
-        for(int i:dp1) cout<<i<<" ";
-        cout<<endl;
-        
         for(int i=n-2;i>=0;i--)
             if(nums[i]>nums[i+1])
                 dp2[i]=dp2[i+1]+1; 
-        
-        for(int i:dp2) cout<<i<<" ";
         
         int lbs=0;
         for(int i=1;i<n-1;i++)
@@ -22,5 +18,19 @@ public:
                 lbs=max(lbs, dp1[i]+dp2[i]-1);
         
         return lbs;
+        */
+        int i=0, ans=0;
+        while(i<n){
+            int up=i+1;            
+            while(up<n && nums[up]>nums[up-1]) up++;
+            int down=up;
+            while(down<n && nums[down]<nums[down-1]) down++;
+            if(up-i>=2 && down-up>=1) ans=max(ans, down-i);        
+            if(down<n && down-1>=0 && nums[down-1]<nums[down]) 
+                i=down-1;
+            else
+                i=down;
+        }
+        return ans;
     }
 };
