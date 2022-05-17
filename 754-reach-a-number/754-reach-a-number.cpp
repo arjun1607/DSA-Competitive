81091cfc5182eedaf28b78859e01373c63604381
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-05-17 08:28:29 +0530

    Time: 6 ms (11.85%), Space: 5.9 MB (69.63%) - LeetHub

---
+++ 754-reach-a-number/754-reach-a-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int reachNumber(int target) {
+        target=abs(target);
+        int i=0, k=1, jumps=0;
+        while(i<target){
+            i+=k; k++; jumps++;
+        }
+        cout<<i<<endl;
+        if(i==target) return jumps;
+        else{
+                while(true){
+                    int diff=i-target;
+                    if(diff%2==0) return  jumps;
+                    i+=k; k++; jumps++;        
+                }
+        }
+    }
+};
