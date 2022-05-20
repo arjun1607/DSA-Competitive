4424f07a75e59853fbb9a36b5d40779d0717f07c
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-05-20 09:42:19 +0530

    Time: 10 ms (55.73%), Space: 7 MB (36.33%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.cpp
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
-    bool check(string text, string ptr){
-        string s=ptr+'#'+text;
-        vector<int> lps(s.size(), 0);
-        int i=1, j=lps[i-1]; 
-        while(i<s.size()){
-            if(s[i]==s[j]){
-                lps[i]=j+1;
-                j++;
-                i++;
-            }else{
-                if(j==0) {
-                    lps[i]=0;
-                    i++;
-                }
-                else j=lps[j-1];
-            }
-        }
-        for(int val:lps) if(val==ptr.size()) return true;
-        return false;
-    }
     int repeatedStringMatch(string a, string b) {
         int n=ceil(b.size()*1.0/a.size());
         string text=a;
         for(int i=1;i<n;i++) text+=a;
-        if(check(text, b)) return n;
+        if(text.find(b)!=string::npos) return n;
         else {
             text+=a;
-            if(check(text, b)) return n+1;
+            if(text.find(b)!=string::npos) return n+1;
         }
         return -1;
     }
