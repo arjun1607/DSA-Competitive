6f54959b63b74430bbd0621c7ecf8a89281a786b
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-05-25 11:51:17 +0530

    Time: 14 ms (98.19%), Space: 11.6 MB (52.43%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,55 +1,22 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string str) {
-        /*
-        int cnt=0;
-        stack<pair<char, int>> s;
-        for(char c:str){
-            if(c=='('){
-                cnt++;
-                s.push({'(', cnt});
-            }else if(c==')'){
-                cnt++;
-                if(!s.empty() && s.top().first=='(') s.pop();
-                else s.push({')', cnt});
+        stack<int> s;
+        for(int i=0;i<str.size();i++){
+            if(str[i]=='(') s.push(i);
+            else if(str[i]==')'){
+                if(!s.empty()) s.pop();
+                else str[i]='#';
             }
         }
-
-        string ans="";
-        int cnt2=0;
-        for(int i=str.size()-1; i>=0; i--){
-            if(str[i]=='(' || str[i]==')'){
-                cnt2++;
-                if(!s.empty() && s.top().first==str[i] 
-                      && cnt-s.top().second+1==cnt2) {
-                    s.pop();
-                    continue;
-                }
-                else ans+=str[i];
- 
-            }else ans+=str[i];
-            
+        while(!s.empty()){
+            str[s.top()]='#';
+            s.pop();
         }
-        reverse(ans.begin(), ans.end());
-        return ans;
-        */
-        stack<char> s;
+        string ans="";
         for(char &c:str){
-            if(c=='(') s.push(c);
-            else if(c==')'){
-                if(!s.empty() && s.top()=='(') s.pop();
-                else c='*';
-            }
+            if(c!='#') ans+=c;
         }
-        string ans="";
-        reverse(str.begin(), str.end());
-        for(char c:str){
-            if(c!='*') {
-                if(!s.empty() && c==s.top()) s.pop();
-                else ans+=c;
-            }
-        }
-        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
