cbb6a18ceb0348410bca6016435ab08d0424e4a9
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-05-30 14:51:15 +0530

    Time: 462 ms (12.72%), Space: 85.8 MB (39.44%) - LeetHub

--- 853-car-fleet/853-car-fleet.cpp
+++ 853-car-fleet/853-car-fleet.cpp
@@ -8,16 +8,28 @@ public:
         }
         sort(time.begin(), time.end());
         
+        stack<double> s;
+        // finding next greater on right
+        for(int i=0;i<n;i++){
+            while(!s.empty() && time[i].second >= time[s.top()].second) s.pop();
+            s.push(i);
+        }
+        return s.size();
+        
+        /*
         int count=1;
         double timetaken_leader=time[n-1].second;
         for(int i=n-2; i>=0; i--){
             double timetaken_currcar = time[i].second;
             cout<<timetaken_currcar<<" "<<timetaken_leader;
+            
+            // if currcar does not collide with any car in front of it
             if(timetaken_currcar > timetaken_leader){
                 timetaken_leader = timetaken_currcar;
                 count++;
             }
         }
         return count;
+        */
     }
 };
