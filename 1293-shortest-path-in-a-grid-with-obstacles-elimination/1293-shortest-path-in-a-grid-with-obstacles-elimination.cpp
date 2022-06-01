8673e38b5e66a55d588da626a9d9173116764388
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-01 21:39:26 +0530

    Time: 334 ms (23.37%), Space: 59.2 MB (14.75%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -4,25 +4,29 @@ public:
         int n=grid.size(), m=grid[0].size();
         vector<vector<vector<bool>>> vis(n, vector<vector<bool>>(m, vector<bool>(k+1, false)));
         queue<vector<int>>  q;
-        q.push({0,0,grid[0][0],0});
+        q.push({0,0,grid[0][0]});
         int x[4] = {-1, 0, 1, 0};
         int y[4] = {0, -1, 0, 1};
+        int steps=0;
         while(!q.empty()){
-            auto t=q.front();
-            q.pop();
-            int i=t[0], j=t[1];
-            int count=t[2], lvl=t[3];
-            if(i==n-1 && j==m-1) return lvl;
-            for(int d=0;d<4;d++){
-                int ni=i+x[d], nj=j+y[d];
-                if(ni>=0 && ni<n && nj>=0 && nj<m){
-                    int newcount = count+grid[ni][nj];
-                    if(newcount<=k && !vis[ni][nj][newcount]){
-                        q.push({ni, nj, newcount, lvl+1});
-                        vis[ni][nj][newcount]=true;
+            int size=q.size();
+            while(size-->0){
+                auto t=q.front();
+                q.pop();
+                int i=t[0], j=t[1], count=t[2];
+                if(i==n-1 && j==m-1) return steps;
+                for(int d=0;d<4;d++){
+                    int ni=i+x[d], nj=j+y[d];
+                    if(ni>=0 && ni<n && nj>=0 && nj<m){
+                        int newcount = count+grid[ni][nj];
+                        if(newcount<=k && !vis[ni][nj][newcount]){
+                            q.push({ni, nj, newcount});
+                            vis[ni][nj][newcount]=true;
+                        }
                     }
                 }
             }
+            steps++;
         }
         return -1;
     }
