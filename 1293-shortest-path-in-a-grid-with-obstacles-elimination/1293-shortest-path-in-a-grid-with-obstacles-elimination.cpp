37025ba5ee1464b793b703f5b0064fb9bc39d37f
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-01 21:31:17 +0530

    Time: 394 ms (18.78%), Space: 59.2 MB (15.07%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int shortestPath(vector<vector<int>>& grid, int allowed) {
+    int shortestPath(vector<vector<int>>& grid, int k) {
         int n=grid.size(), m=grid[0].size();
-        vector<vector<vector<bool>>> vis(n, vector<vector<bool>>(m, vector<bool>(allowed+1, false)));
+        vector<vector<vector<bool>>> vis(n, vector<vector<bool>>(m, vector<bool>(k+1, false)));
         queue<vector<int>>  q;
         q.push({0,0,grid[0][0],0});
         int x[4] = {-1, 0, 1, 0};
@@ -13,11 +13,11 @@ public:
             int i=t[0], j=t[1];
             int count=t[2], lvl=t[3];
             if(i==n-1 && j==m-1) return lvl;
-            for(int k=0;k<4;k++){
-                int ni=i+x[k], nj=j+y[k];
+            for(int d=0;d<4;d++){
+                int ni=i+x[d], nj=j+y[d];
                 if(ni>=0 && ni<n && nj>=0 && nj<m){
                     int newcount = count+grid[ni][nj];
-                    if(newcount<=allowed && !vis[ni][nj][newcount]){
+                    if(newcount<=k && !vis[ni][nj][newcount]){
                         q.push({ni, nj, newcount, lvl+1});
                         vis[ni][nj][newcount]=true;
                     }
