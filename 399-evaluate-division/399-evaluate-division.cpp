3b1ed8797c1fed95fd7e29954bfc9adc462d7417
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-02 17:12:38 +0530

    Time: 5 ms (31.79%), Space: 8.4 MB (22.47%) - LeetHub

---
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<double> ans;
+    void dfs(unordered_map<string, unordered_map<string, double>> &adj, 
+             string src, string des, unordered_map<string, bool> &vis, double cost, bool &found){
+        if(src==des && found==false){
+            found=true;
+            ans.push_back(cost);
+            return;
+        }
+        vis[src]=true;
+        for(auto i:adj[src]){
+            if(!vis[i.first]){
+                dfs(adj, i.first, des, vis, cost*i.second, found);
+            }
+        }
+    }
+    vector<double> calcEquation(vector<vector<string>>& eq, 
+                                vector<double>& val, vector<vector<string>>& q) {
+        unordered_map<string, unordered_map<string, double>> adj;
+        for(int i=0;i<eq.size();i++){
+            auto var=eq[i];
+            adj[var[0]][var[1]]=val[i];
+            adj[var[1]][var[0]]=(1/val[i]);
+        }
+        for(auto i:q){
+            string var1=i[0], var2=i[1];
+            unordered_map<string, bool> vis;
+            if(adj.find(var1)==adj.end() || adj.find(var2)==adj.end()){
+                ans.push_back(-1.0);
+            }else{
+                bool found=false;
+                dfs(adj, var1, var2, vis, 1, found);
+                if(!found) ans.push_back(-1.0);
+            }
+        }
+        return ans;
+    }
+};
