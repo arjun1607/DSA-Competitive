9e81f79a4ba70167a76d93c817d4d461370bf3a3
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-02 09:11:20 +0530

    Time: 11 ms (88.86%), Space: 10.7 MB (43.03%) - LeetHub

---
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& grid) {
+        int n=grid.size(), m=grid[0].size();
+        vector<vector<int>> ans(m, vector<int>(n));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                ans[j][i]=grid[i][j];
+            }
+        }
+        return ans;
+    }
+};
