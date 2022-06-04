c65df36c67fd61660639948d95ebb7317e5ef9fa
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-04 21:00:55 +0530

    Time: 13 ms (43.11%), Space: 7.4 MB (55.22%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     vector<vector<string>> ans;
-    void helper(vector<vector<int>> &chess, int row, vector<bool> &col, vector<bool> &nd, 
-                              vector<bool> &rd){
-        if(row==chess.size()){
-            vector<string> temp;
-            for(int i=0;i<chess.size();i++){
-                string s=""; 
-                for(int j=0;j<chess[0].size();j++){
-                    if(chess[i][j]==0) s+='.';
-                    else s+='Q';
-                }
-                temp.push_back(s);
-            }
-            ans.push_back(temp);
+    bool isvalid(int x, int y, vector<string> &grid){
+        for(int i=x-1;i>=0;i--){
+            if(grid[i][y]=='Q') return false;
+        }
+        for(int i=x-1, j=y-1; i>=0 && j>=0; i--, j--){
+            if(grid[i][j]=='Q') return false;
+        }
+        for(int i=x-1, j=y+1; i>=0 && j<grid[0].size(); i--, j++){
+            if(grid[i][j]=='Q') return false;
+        }
+        return true;
+    }
+    void helper(int i, vector<string> &grid, int n){
+        if(i==n){
+            ans.push_back(grid);
             return;
         }
-        for(int i=0;i<chess[0].size();i++){
-            if(col[i]==false && nd[i+row]==false && rd[row-i+chess.size()-1]==false){
-                chess[row][i]=1;
-                col[i]=true;
-                nd[row+i]=true;
-                rd[row-i+chess.size()-1]=true;
-                
-                helper(chess, row+1, col, nd, rd);
-                
-                chess[row][i]=0;
-                col[i]=false;
-                nd[row+i]=false;
-                rd[row-i+chess.size()-1]=false;
+            for(int j=0;j<n;j++){
+                if(isvalid(i, j, grid)){
+                    grid[i][j]='Q';
+                    helper(i+1, grid, n);
+                    grid[i][j]='.';
+                }
             }
-        }
+        
     }
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<int>> chess(n, vector<int>(n,0));
-        vector<bool> col(n, false), nd(2*n-1, false), rd(2*n-1, false);
-        helper(chess, 0, col, nd, rd);
+        vector<string> grid(n, string(n, '.'));
+        helper(0, grid, n);
         return ans;
     }
 };
