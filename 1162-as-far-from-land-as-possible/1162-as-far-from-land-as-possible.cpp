73e0e5c2b57f01692a0ef9602a50e00347349ca4
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-05 17:57:47 +0530

    Time: 190 ms (15.94%), Space: 48.8 MB (9.26%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {      
-        int n=grid.size();
+        int n=grid.size(), m=grid[0].size();
         queue<vector<int>> q;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1) {
-                    q.push({i, j, 0});
-                }
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) q.push({i, j});
             }
         }
         int ans=INT_MIN;
@@ -19,10 +17,10 @@ public:
             int i=t[0], j=t[1], lvl=t[2];
             for(int k=0;k<4;k++){
                 int ni=i+x[k], nj=j+y[k];
-                if(ni>=0 && ni<n && nj>=0 && nj<n && grid[ni][nj]==0){
-                    grid[ni][nj]=grid[i][j]+1;
-                    ans=max(ans, grid[ni][nj]);
-                    q.push({ni, nj, lvl+1});
+                if(ni>=0 && ni<n && nj>=0 && nj<m && grid[ni][nj]==0){
+                       grid[ni][nj]=grid[i][j]+1;
+                       ans=max(ans, grid[ni][nj]);
+                       q.push({ni, nj});
                 } 
             }
         }
