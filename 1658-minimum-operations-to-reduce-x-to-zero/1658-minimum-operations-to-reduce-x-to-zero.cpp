4b1e20d839cc7dabeba6c66af66f4e3937c51d7a
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-11 13:49:51 +0530

    Time: 292 ms (56.01%), Space: 98.5 MB (90.95%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int s=0;
+        for(int i:nums) s+=i;
+        x=s-x;
+
+        // now finding max subarray whose sum is x
+        int i=0, j=0, size=-1, sum=0;
+        while(j<nums.size()){
+            sum+=nums[j];
+            if(sum==x){
+                size=max(size, j-i+1);
+            }else if(sum>x){
+                while(sum>x && i<nums.size()){
+                    sum-=nums[i];
+                    i++;
+                }
+               if(sum==x) size=max(size, j-i+1);
+            }
+            j++;
+        }
+        
+        if(size==-1) return -1;
+        
+        return nums.size()-size;
+    }
+};
