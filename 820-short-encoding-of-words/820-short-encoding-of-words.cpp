df2e4568c0da7e802d5e39405e105cc56b0dd5a3
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-20 23:04:30 +0530

    Time: 139 ms (42.08%), Space: 73.8 MB (29.70%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,60 +1,45 @@
-// class Node{
-//     public:
-//     Node *links[26];
-//     bool flag;
-//     bool containsKey(char ch){
-//         return links[ch-'a']!=NULL;
-//     }
-//     void put(char ch, Node* node){
-//         links[ch-'a'] = node;
-//     }
-//     Node* get(char ch){
-//         return links[ch-'a'];
-//     }
-// };
+class Node{
+    public:
+    Node *links[26];
+    bool containsKey(char ch){
+        return links[ch-'a']!=NULL;
+    }
+    void put(char ch, Node* node){
+        links[ch-'a'] = node;
+    }
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+};
 class Solution {
 public:
+    int ans=0;
+    void dfs(Node *node, int len){
+        bool leaf=true;
+        for(int i=0;i<26;i++){
+            if(node->links[i]!=NULL){
+                leaf=false;
+                dfs(node->links[i], len+1);
+            }
+        }
+        if(leaf){
+            ans+=(len+1);
+        }
+    }
     int minimumLengthEncoding(vector<string>& words) {
-        /*
         Node *root = new Node();
-        int ans=0;
-        for(int i=0;i<words.size();i++){
-            string s=words[i];
-            reverse(s.begin(), s.end());
-            Node *node = root;
-            int temp=0;
-            bool longer=false;
-            for(int j=0;j<s.size();j++){
-                if(!node->containsKey(s[j])){
-                    node->put(s[j], new Node());
-                    if(j==s.size()-1){
-                        longer=true;
-                    }
+        // inserting in trienode
+        for(auto &s:words){
+            Node *node=root;
+            for(int i=s.size()-1; i>=0;i--){
+                if(!node->containsKey(s[i])){
+                    node->put(s[i], new Node());
                 }
-                temp++;
-                if(node->flag==true) temp=0;
-                node=node->get(s[j]);
-            }
-            node->flag=true;
-            if(longer){
-                ans+=(temp+1);
+                node = node->get(s[i]);
             }
         }
-        return ans;
-        */
-        set<string> s1(words.begin(), words.end());
-        set<string> s2(words.begin(), words.end());   
-        for(auto &s:s1){
-            for(int i=1;i<s.size();i++){
-                if(s2.find(s.substr(i)) != s2.end()){
-                    s2.erase(s.substr(i));
-                }
-            }
-        }
-        int ans=0;
-        for(auto &s:s2){
-            ans+=(s.size()+1);
-        }
+        
+        dfs(root, 0);
         return ans;
     }
 };
