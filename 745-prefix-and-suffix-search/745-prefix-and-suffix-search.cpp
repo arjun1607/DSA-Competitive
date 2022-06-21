3a3c08e2795196f2ab5b5aedee569f80d8d1ae06
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-21 14:31:35 +0530

    Time: 954 ms (49.39%), Space: 266 MB (5.00%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -1,6 +1,46 @@
+class Node
+{
+    public:
+    Node *links[27]={NULL};
+    int maxidx=-1;
+    
+    bool containsKey(char ch)
+    {
+        return links[ch-'a']!=NULL;
+    }
+    void put(char ch,Node *node)
+    {
+        links[ch-'a']=node;
+    }
+    Node *get(char ch)
+    {
+        return links[ch-'a'];
+    }  
+};
+
 class WordFilter {
 public:
-    unordered_map<string, int>  map;
+    Node *root = new Node();
+    void insert(string s, int k){
+        Node *node = root;
+        for(int i=0;i<s.size();i++){
+            if(!node->containsKey(s[i])){
+                node->put(s[i], new Node());
+            }
+            node = node->get(s[i]);
+        }
+        node->maxidx = k;
+    }
+    int search(string s){
+        Node *node = root;
+        for(int i=0;i<s.size();i++){
+            if(!node->containsKey(s[i])){
+                return -1;
+            }
+            node = node->get(s[i]);
+        }
+        return node->maxidx;
+    }
     WordFilter(vector<string>& words) {
         for(int k=0;k<words.size();k++){
             string s=words[k], pre="";
@@ -9,16 +49,19 @@ public:
                 string suf="";
                 for(int j=s.size()-1; j>=0; j--){
                     suf = s[j] + suf;
-                    map[pre+'#'+suf] = k;
+                    insert(pre+'{'+suf, k);
                 }
             }
         }
     }
     
     int f(string pre, string suf) {
-        if(map.find(pre+'#'+suf) == map.end()) return -1;
-        return map[pre+'#'+suf];
+        return search(pre+'{'+suf);
     }
 };
 
-// brute force => take every string, for all prefixes take all possible suffixes and store in hashmap with index
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter* obj = new WordFilter(words);
+ * int param_1 = obj->f(prefix,suffix);
+ */
