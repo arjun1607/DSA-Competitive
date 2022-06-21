f9fb84ccd715a89ea0936fbc77168d9638518da5
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-21 12:51:50 +0530

    Time: 987 ms (46.29%), Space: 151.1 MB (37.76%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -1,73 +1,24 @@
-#define ll long long
-#define vb vector<bool>
-#define vi vector<int>
-#define vl vector<long long>
-#define vvb vector<vector<bool>>
-#define vvi vector<vector<int>>
-#define vvl vector<vector<long long>>
-#define pii pair<int,int>
-#define all(i) i.begin(),i.end()
-#define loopf(i,s,e) for(int i = s; i < e; i++)
-#define loopb(i,s,e) for(int i = s; i >= e; i--)
-inline int getMid(int i,int j) { return i+(j-i)/2; }
-
-struct TrieNode {
-    unordered_map<char,TrieNode*> children;
-    int idx = -1;
-};
-
-struct Trie {
-    TrieNode* root;
-
-    Trie(vector<string>& words) {
-        this->root = new TrieNode();
-        loopf(i,0,words.size()) {
-            string word = "#"+words[i];
-            loopb(j,words[i].length()-1,0) {
-                word = words[i][j]+word;
-                insert(word,i);
+class WordFilter {
+public:
+    unordered_map<string, int>  map;
+    WordFilter(vector<string>& words) {
+        for(int k=0;k<words.size();k++){
+            string s=words[k], pre="";
+            for(int i=0;i<s.size();i++){
+                pre+=s[i];
+                string suf="";
+                for(int j=s.size()-1; j>=0; j--){
+                    suf = s[j] + suf;
+                    map[pre+'#'+suf] = k;
+                }
             }
         }
     }
-
-    void insert(string& s, int index) {
-        auto p = root;
-        loopf(i,0,s.length()) {
-            if(p->children.count(s[i]) == 0) p->children[s[i]] = new TrieNode();
-            p = p->children[s[i]];
-        }
-        p->idx = index;
-    }
-
-    int query(string& pref, string& suff) {
-        string s = suff+"#"+pref;
-        auto p = root;
-        loopf(i,0,s.length()) {
-            if(p->children.count(s[i]) == 0) return -1;
-            p = p->children[s[i]];
-        }
-
-        return dfs(p);
-    }
-
-    int dfs(TrieNode* root) {
-        int res = root->idx;
-        for(auto i:root->children) {
-            res = max(res,dfs(i.second));
-        }
-        return res;
-    }
-};
-
-class WordFilter {
-public:
-    Trie *t;
-
-    WordFilter(vector<string>& words) {
-        this->t = new Trie(words);
-    }
     
-    int f(string prefix, string suffix) {
-        return t->query(prefix,suffix);
+    int f(string pre, string suf) {
+        if(map.find(pre+'#'+suf) == map.end()) return -1;
+        return map[pre+'#'+suf];
     }
 };
+
+// brute force => take every string, for all prefixes take all possible suffixes and store in hashmap with index
