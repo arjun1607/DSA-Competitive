e87642b0c2e8a619d58a65dbd445d62b5cbdab93
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-23 17:13:49 +0530

    Time: 238 ms (80.50%), Space: 224.2 MB (21.04%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.cpp
+++ 472-concatenated-words/472-concatenated-words.cpp
@@ -1,19 +1,61 @@
+class Node{
+    public:
+    Node *links[26]={NULL};
+    bool flag;
+    bool containsKey(char ch){
+        return links[ch-'a']!=NULL;
+    }
+    void put(char ch, Node *node){
+        links[ch-'a'] = node;
+    }
+    Node *get(char ch){
+        return links[ch-'a'];
+    }
+};
 class Solution {
 public:
-    set<string> st;
+    Node *root = new Node();
+    void insert(string word) {
+        Node *node = root;
+        for(int i=0;i<word.size();i++){
+            if(!node->containsKey(word[i])){
+                node->put(word[i], new Node());
+            }
+            node = node->get(word[i]);
+        }
+        node->flag=true;
+    }
+    bool search(string word) {
+        Node *node=root;
+        for(int i=0;i<word.size();i++){
+            if(!node->containsKey(word[i])){
+                return false;
+            }
+            node = node->get(word[i]);
+        }
+        return node->flag;
+    }
+    
     bool isConcat(string s){
-        string pre="";
-        for(int i=1;i<s.size();i++){
-            string suffix=s.substr(i);
-            if(st.find(s.substr(0,i))!=st.end() && (st.find(suffix)!=st.end() || isConcat(suffix))){
-                return true;
+        Node *node = root;
+        for(int i=0;i<s.size();i++){
+            if(!node->containsKey(s[i])){
+                return false;
+            }
+            node = node->get(s[i]);
+            if(node->flag==true){
+                string suffix = s.substr(i+1);
+                if(search(suffix) || isConcat(suffix)){
+                    return true;
+                }
             }
         }
         return false;
     }
+    
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
         for(auto &s:words){
-            st.insert(s);
+            insert(s);
         }
         vector<string> ans;
         for(auto &s:words){
