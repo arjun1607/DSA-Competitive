bd7010202b3deaca28efcb70b91e73bb4706651b
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-24 14:13:23 +0530

    Time: 21 ms (28.72%), Space: 12.6 MB (32.65%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -2,11 +2,9 @@ class Solution {
 public:
     vector<int> grayCode(int n) {
         if(n==1) return {0,1};      
-        vector<int> recans = grayCode(n-1);
-        vector<int> myans(recans.begin(), recans.end());
-        for(int i=recans.size()-1;i>=0;i--){
-            myans.push_back(recans[i] | (1<<(n-1)));
-        }
-        return myans;
+        vector<int> t= grayCode(n-1);
+        vector<int> v(t);
+        for(int i=t.size()-1;i>=0;i--) v.push_back(t[i] | (1<<(n-1)));
+        return v;
     }
 };
