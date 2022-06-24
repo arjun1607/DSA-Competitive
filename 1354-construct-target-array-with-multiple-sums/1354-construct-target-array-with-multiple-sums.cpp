dce7487cd3bde0d7b882665b2e7919a0c2412433
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-24 10:48:30 +0530

    Time: 70 ms (42.66%), Space: 32.4 MB (27.27%) - LeetHub

--- 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
+++ 1354-construct-target-array-with-multiple-sums/1354-construct-target-array-with-multiple-sums.cpp
@@ -12,11 +12,16 @@ public:
             long restsum = sum - val;    
             if(restsum>=val || restsum<1) return false;
             
-            long old=val%restsum; // to reduce time complexity
-            // suppose s
+            long old=val%restsum; // % to reduce time complexity
+            // suppose val=1001, restsum=2
+            // if we reduce 2 each time like 1001->999->997->......->1 this will take so much time
+            // we can directly do %, and get 1001 -> 1 in one go
             
             sum = restsum + old;
             pq.pop();
+            
+            // suppose val=25, restsum=5
+            // by doing %, we will get old = 0 but we should get 5 by eventually reducing 25->20->15->10->5
             pq.push(old>0?old:sum);
         }
         return true;
