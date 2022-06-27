29398c79f6418ee7b4725a91f750aea02275ed79
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-27 14:24:15 +0530

    Time: 12 ms (59.27%), Space: 9 MB (73.10%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void solve(int currbox, vector<int> &temp, int totbox, unordered_map<int,int> &freq){
-        if(currbox==totbox){
+    void solve(int currnoidx, vector<int> &temp, vector<int> &nums, unordered_map<int,int> &lastoccur){
+        if(currnoidx==temp.size()){
             ans.push_back(temp);
+            return;
         }
-        
-        for(auto &it:freq){
-            int val=it.first;
-            if(it.second > 0){
-                it.second--;
-                temp.push_back(val);
-                solve(currbox+1, temp, totbox, freq);
-                temp.pop_back();
-                it.second++;
+  
+        int val=nums[currnoidx];
+        int lo = lastoccur[val];
+  
+        for(int i=lo+1; i<temp.size();i++){
+            if(temp[i]==INT_MIN){
+                lastoccur[val]=i;
+                temp[i]=val;
+                solve(currnoidx+1, temp, nums, lastoccur);
+                lastoccur[val]=lo;
+                temp[i]=INT_MIN;
             }
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        for(int i:nums) freq[i]++;
-        
+        unordered_map<int, int> lastoccur;
+        for(int i:nums) lastoccur[i]=-1;
         int n=nums.size();
-        vector<int> temp;
-        solve(0, temp, n, freq);
+        vector<int> temp(n, INT_MIN);
+        solve(0, temp, nums, lastoccur);
         return ans;
     }
 };
