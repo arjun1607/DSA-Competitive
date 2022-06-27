accf6107b349d3b72d3e324b09115d899df88c01
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-27 17:44:04 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (21.36%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void helper(int i, int nosleft, int target, vector<int> &temp, int currsum){
-        if(nosleft==0){
-            if(currsum==target){
-                ans.push_back(temp);
-            }
+    void solve(int i, int remainingSum, int ssf, int ts, vector<int> &temp){
+        if(i==10){
+            if(remainingSum==0 && ssf==ts) ans.push_back(temp);
             return;
-        }  
-        if(i==10) return ;
-        
-        // include
-        if(i<=target){
-            temp.push_back(i);
-            helper(i+1, nosleft-1, target, temp, currsum+i);
-            temp.pop_back();
         }
         
-        // not include
-        helper(i+1, nosleft, target, temp, currsum);
+        solve(i+1, remainingSum, ssf, ts, temp);
         
+        if(i<=remainingSum){
+            temp.push_back(i);
+            solve(i+1, remainingSum-i, ssf+1, ts, temp);
+            temp.pop_back();
+        }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int> temp;
-        helper(1, k, n, temp, 0);
+        solve(1, n, 0, k, temp);
         return ans;
     }
 };
