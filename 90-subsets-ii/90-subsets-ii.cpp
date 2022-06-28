ad7cf24b9b40302b6edc96407bfd9f9dfc7f448e
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-28 07:00:50 +0530

    Time: 0 ms (100.00%), Space: 11.6 MB (27.15%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n=nums.size();
-        set<vector<int>> s;
-        for(int i=0;i<(1<<n);i++){
-            vector<int> temp;
-            for(int j=0;j<n;j++){
-                if(i&(1<<j)){
-                    temp.push_back(nums[j]);
-                }
-            }
-            sort(temp.begin(), temp.end());
-            s.insert(temp);
+    vector<vector<int>> ans;
+    void solve(int idx, vector<int> &unique, unordered_map<int, int> &fmap, vector<int> &temp){
+        if(idx==unique.size()){
+            ans.push_back(temp);
+            return;
         }
-        vector<vector<int>> ans;
-        for(auto i:s) ans.push_back(i);
+        
+        int val=unique[idx];
+        for(int i=1; i<= fmap[val]; i++){
+            for(int j=0;j<i;j++) temp.push_back(val);
+            solve(idx+1, unique, fmap, temp);
+            for(int j=0;j<i;j++) temp.pop_back();
+        }
+        
+        solve(idx+1, unique, fmap, temp);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        unordered_map<int, int> fmap;
+        vector<int> unique;
+        for(int i:nums){
+            if(fmap.find(i)==fmap.end()){
+                unique.push_back(i);
+            }
+            fmap[i]++;
+        }
+        
+        vector<int> temp;
+        solve(0, unique, fmap, temp);
         return ans;
     }
 };
