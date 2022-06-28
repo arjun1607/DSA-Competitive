2259b7607d22644857f5b76c5a61aca5b40c38fc
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-06-28 07:12:52 +0530

    Time: 15 ms (31.18%), Space: 14.7 MB (15.64%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    vector<vector<int>> ans;
+    void solve(int idx, vector<int> &unique, unordered_map<int, int> &fmap, vector<int> &temp, int target){
+        if(target==0){
+            ans.push_back(temp);
+            return;
+        }
+        if(idx==unique.size()){
+            if(target==0) ans.push_back(temp);
+            return;
+        } 
+        
+        int val=unique[idx];
+        for(int i=1; i<= fmap[val]; i++){
+            for(int j=0;j<i;j++) {
+                temp.push_back(val);
+                target-=val;
+            }
+            if(target>=0) solve(idx+1, unique, fmap, temp, target);
+            for(int j=0;j<i;j++) {
+                temp.pop_back();
+                target+=val;
+            }
+        }
+        
+        solve(idx+1, unique, fmap, temp, target);
+    }
+    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {                                                 unordered_map<int, int> fmap;
+        vector<int> unique;
+        for(int i:nums){
+            if(fmap.find(i)==fmap.end()){
+                unique.push_back(i);
+            }
+            fmap[i]++;
+        }
+        
+        vector<int> temp;
+        solve(0, unique, fmap, temp, target);
+        return ans;
+    }
+};
