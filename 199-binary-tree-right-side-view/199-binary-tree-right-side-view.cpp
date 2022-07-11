9c32a44d336a01dff947ff32eaab7631a240140f
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-07-11 06:41:19 +0530

    Time: 6 ms (50.04%), Space: 12 MB (66.66%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -18,12 +18,12 @@ public:
         vector<int> ans;
         while(!q.empty()){
             int size=q.size();
-            while(size-- > 0){
+            ans.push_back(q.front()->val);
+            while(size--){
                 auto node = q.front();
-                if(size==0) ans.push_back(node->val);
                 q.pop();
-                if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
+                if(node->left) q.push(node->left);
             }
         }
         return ans;
