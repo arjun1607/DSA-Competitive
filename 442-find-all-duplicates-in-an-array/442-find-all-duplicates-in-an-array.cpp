b2ac194c26c85a404d50095d6ad9e552301be6e0
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-07-17 13:00:33 +0530

    Time: 113 ms (39.34%), Space: 33.5 MB (92.78%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        /*
-        we Can Use -[minus] To track Each Element we are traverse Or Not [as They said That any element can occoure max Twice]
-While Traverseing if We Get Negetive element again We Know That It's Allredy Visited So It Wolud Be a duplicate num <= Push That Into ANS array
-        */
-        vector<int> ans;
-        for(int i:nums){
-            if(nums[abs(i)-1]<0) ans.push_back(abs(i));
-            else nums[abs(i)-1] = -abs(nums[abs(i)-1]);
+         vector<int> ans;
+        int i=0;
+        while(i<nums.size()){
+            if(nums[nums[i]-1] != nums[i]) 
+                swap(nums[i], nums[nums[i]-1]);
+            else i++;
+        }    
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=i+1) ans.push_back(nums[i]);
         }
         return ans;
     }
