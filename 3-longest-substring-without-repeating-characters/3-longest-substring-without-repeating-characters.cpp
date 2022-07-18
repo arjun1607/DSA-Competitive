178f7241218ea811c62670b08aadb485270dac36
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-07-18 18:30:24 +0530

    Time: 27 ms (51.38%), Space: 8.9 MB (37.60%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -5,14 +5,15 @@ public:
         int i=0, j=0, ans=0;
         while(j<s.size()){
             m[s[j]]++;
-            if(m.size() == j-i+1){
+            if(m.size()==j-i+1){
                 ans=max(ans, j-i+1);
-            }else if(m.size() < j-i+1){
-                while(m.size() < j-i+1){
+            }else if(m.size()<j-i+1){
+                while(m.size()<j-i+1){
                     m[s[i]]--;
                     if(m[s[i]]==0) m.erase(s[i]);
                     i++;
                 }
+                ans=max(ans, j-i+1);
             }
             j++;
         }
