3809284d24f5f7b53767aab23db34f757fe2d2b0
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-07-30 11:43:24 +0530

    Time: 1175 ms (31.70%), Space: 130.1 MB (28.99%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     int mst(vector<pair<int,int>> *adj, int n){
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         vector<bool> vis(n, false);
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
         pq.push({0,0});
-        int cost=0;
+        int ans=0;
         while(!pq.empty()){
             auto t=pq.top();
             pq.pop();
-            int weight=t.first;
-            int vertex=t.second;
+            int weight = t.first;
+            int vertex = t.second;
             if(!vis[vertex]){
-                cost+=weight;
-                vis[vertex]=true;
+                vis[vertex] = true;
+                ans += weight;
                 for(auto i:adj[vertex]){
-                    if(!vis[i.first]){
-                        pq.push({i.second, i.first});
+                    int neigh = i.first;
+                    int neighW = i.second;
+                    if(!vis[neigh]){
+                        pq.push({neighW , neigh});
                     }
                 }
             }
         }
-        return cost;
+        return  ans;
     }
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n=points.size();
         vector<pair<int,int>> adj[n];
-        for(int i=0;i<n-1;i++){
+        for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                int dis = abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1]);
-                adj[i].push_back({j, dis});
-                adj[j].push_back({i, dis});
+                int dist = abs(points[i][0]-points[j][0]) + abs(points[i][1]-points[j][1]);
+                adj[i].push_back({j, dist});
+                adj[j].push_back({i, dist});
             }
         }
         return mst(adj, n);
