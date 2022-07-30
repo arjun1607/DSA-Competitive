3982823d05bd75dd2e78b4133ed9475002c203fb
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-07-30 08:29:04 +0530

    Time: 219 ms (40.98%), Space: 86.4 MB (11.60%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int ans=0;
-    void dfs(TreeNode *root, multiset<int> &m){
+    void dfs(TreeNode *root, int maxi){
         if(root==NULL) return ;
-        if(m.upper_bound(root->val)==m.end()) ans++;
-        m.insert(root->val);
-        dfs(root->left, m);
-        dfs(root->right, m);
-        auto it=m.find(root->val);
-        m.erase(it);
+        if(root->val >= maxi) ans++;
+        maxi=max(maxi, root->val);
+        dfs(root->left, maxi);
+        dfs(root->right, maxi);
     }
-    int goodNodes(TreeNode* root) {
-        multiset<int> m;
-        dfs(root, m);
+    int goodNodes(TreeNode* root){
+        dfs(root, -1e8);
         return ans;
     }
 };
