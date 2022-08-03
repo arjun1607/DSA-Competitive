3f7310975c15d91e56d46af42e919fbffb6e5796
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-08-03 16:16:47 +0530

    Time: 14 ms (85.74%), Space: 10.3 MB (33.42%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.cpp
+++ 130-surrounded-regions/130-surrounded-regions.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-    void dfs(vector<vector<char>>& grid, int i, int j, vector<vector<bool>> &vis){
-        if(i<0 || i==grid.size() || j<0 || j==grid[0].size() || vis[i][j] || grid[i][j]=='X') return;
+    void dfs(int i, int j, vector<vector<char>> &grid, vector<vector<bool>> &vis){
+        if(i<0 || j<0 || i==grid.size() || j==grid[0].size() || vis[i][j] || grid[i][j]=='X') return;
         vis[i][j]=true;
-        dfs(grid, i-1, j, vis);
-        dfs(grid, i, j-1, vis);
-        dfs(grid, i+1, j, vis);
-        dfs(grid, i, j+1, vis);
+        dfs(i+1, j, grid, vis);
+        dfs(i-1, j, grid, vis);
+        dfs(i, j+1, grid, vis);
+        dfs(i, j-1, grid, vis);
     }
     void solve(vector<vector<char>>& grid) {
         int n=grid.size(), m=grid[0].size();
+        
         vector<vector<bool>> vis(n, vector<bool>(m, false));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if((i==0 || i==n-1 || j==0 || j==m-1) && grid[i][j]=='O' && !vis[i][j])
-                    dfs(grid, i, j, vis);
+                if((i==0 || j== 0 || i==n-1 || j==m-1)  &&  grid[i][j]=='O'){
+                    dfs(i, j, grid, vis);
+                } 
             }
         }
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]=='O' && vis[i][j]==false)
-                    grid[i][j]='X';
+                if(grid[i][j]=='O' && !vis[i][j]){
+                    grid[i][j] = 'X';
+                } 
             }
         }
+        
     }
 };
