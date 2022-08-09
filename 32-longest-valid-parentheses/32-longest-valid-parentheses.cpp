57cf13e096e3cd5fd414c5fe84a0b5738def72d5
Arjun <86774036+arjun1607@users.noreply.github.com>
2022-08-09 12:29:33 +0530

    Time: 4 ms (78.35%), Space: 7.4 MB (41.82%) - LeetHub

--- 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/32-longest-valid-parentheses.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     int longestValidParentheses(string str) {
         stack<int> s;
-        // stack will contain unpaired brackets
-        int ans=0, i=0;
-        while(i<str.size()){
+        int maxi = 0;
+        for(int i=0;i<str.size();i++){
             if(str[i]=='(') s.push(i);
             else{
                 if(!s.empty() && str[s.top()]=='('){
                     s.pop();
-                    int last_unpaired=s.empty()?-1:s.top();
-                    ans=max(ans, i-last_unpaired);
+                    int val = s.empty() ? -1 : s.top();
+                    maxi=max(maxi, i-val);
                 }
                 else s.push(i);
             }
-            i++;
         }
-        return ans;
+        return maxi;
     }
 };
